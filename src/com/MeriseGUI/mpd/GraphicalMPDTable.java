54b1a190a6bba2c9e58cf57054520a8e07638c29
Massinissa Ousmaal <ousmaalmassi@gmail.com>
2022-06-06 09:41:09 +0100

    refactor drawAttributes method in GraphicalMPDTable

--- src/com/MeriseGUI/mpd/GraphicalMPDTable.java
+++ src/com/MeriseGUI/mpd/GraphicalMPDTable.java
@@ -17,6 +17,8 @@ public class GraphicalMPDTable extends GraphicalNode {
     private int attrNbr;
     private List<Property> primaryKeys;
     private List<Property> foreignKeys;
+    private int sx;
+    private int sy;
 
     public GraphicalMPDTable(int x, int y, String name) {
         super(x, y, name);
@@ -50,49 +52,28 @@ public class GraphicalMPDTable extends GraphicalNode {
         g.drawRect(pulledX, pulledY, width, height);
         g.drawLine(pulledX, pulledY+headHeight, pulledX+width, pulledY+headHeight);
 
-        int sx = pulledX + PADDING;
-        int sy = pulledY + headHeight + PADDING;
+        sx = pulledX + PADDING;
+        sy = pulledY + headHeight + PADDING;
         int lineHeight = fm.getHeight() + 3;
         int nameY = pulledY + lineHeight;
 
 //        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 17));
         g.drawString(name, sx, nameY);
 
-        drawAttributes(g, sx, sy);
-    }
-    private void drawAttributes(Graphics2D g, int sx, int sy) {
 
-       /* for (Property property : primaryKeys) {
-            drawConstraintHeader("Pk_Primary", property.code, g, sx, sy);
-            sy += SPACING;
-
-            String line = property.code+" "+property.type+"("+property.length+")";
-            g.drawString(line, sx, sy);
-            sy += SPACING;
-        }*/
-
-        drawConstraint("Pk_Primary", g, sx, sy);
-
-        for (Property property : attributes) {
-            String line = property.code+" "+property.type+"("+property.length+")";
-            g.drawString(line, sx, sy);
-            sy += SPACING;
-        }
-
-        for (Property property : foreignKeys) {
-            drawConstraint("Pk_Foreign", g, sx, sy);
-            sy += SPACING;
-            String line = property.code+" "+property.type+"("+property.length+")";
-            g.drawString(line, sx, sy);
-            sy += SPACING;
-        }
+        drawAttributes("Pk_Primary", primaryKeys, g);
+        drawAttributes("Attributes", attributes, g);
+        drawAttributes("Pk_Foreign", foreignKeys, g);
     }
 
-    private void drawConstraint(String constName, Graphics2D g, int sx, int sy) {
+    private void drawAttributes(String constName, List<Property> list, Graphics2D g) {
+        if (list.isEmpty())
+            return;
 
         int lineX1 = sx - 5;
         int vLineY1 = sy;
         int lineY2 = sy;
+        int hLineX2 = 0;
         int maxLine = 0;
 
         g.setFont(new Font(Font.DIALOG, Font.ITALIC, 11));
@@ -100,16 +81,17 @@ public class GraphicalMPDTable extends GraphicalNode {
         g.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
         sy += SPACING;
 
-        for (Property property : primaryKeys) {
+        for (Property property : list) {
             String line = property.code+" "+property.type+"("+property.length+")";
             g.drawString(line, sx, sy);
             sy += SPACING;
             lineY2 += 20;
             maxLine = Math.max(fm.stringWidth(property.code), maxLine);
         }
-        int hLineX2 = sx + maxLine;
+        hLineX2 = sx + maxLine;
         g.drawLine(lineX1, vLineY1, lineX1, lineY2);
         g.drawLine(lineX1, lineY2, hLineX2, lineY2);
+
     }
 
     private int calculateWidth(Graphics2D g){
